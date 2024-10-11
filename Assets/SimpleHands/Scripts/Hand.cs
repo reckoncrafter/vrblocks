5d7eec2c9604423af043be68099d29f1e53e9bdd
Tyler Braun <tyler.braun@toyota.com>
2024-10-11 15:47:08 -0500

    removed unused curr values in Hand

--- Assets/SimpleHands/Scripts/Hand.cs
+++ Assets/SimpleHands/Scripts/Hand.cs
@@ -7,10 +7,6 @@ public class Hand : MonoBehaviour
   [SerializeField] private InputActionReference gripAction;
   [SerializeField] private InputActionReference triggerAction;
   Animator animator;
-
-  private float currGrip;
-  private float currTrigger;
-
   private readonly string animatorGripParam = "Grip";
   private readonly string animatorTriggerParam = "Trigger";
 
@@ -29,11 +25,6 @@ public class Hand : MonoBehaviour
 
     float grip = gripAction.action.ReadValue<float>();
     float trigger = triggerAction.action.ReadValue<float>();
-    if (currGrip != grip || currTrigger != trigger)
-    {
-      currGrip = grip;
-      currTrigger = trigger;
-    }
 
     animator.SetFloat(animatorGripParam, grip);
     animator.SetFloat(animatorTriggerParam, trigger);
