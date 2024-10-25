1c5c5f866bbc022a20063a70e4dbc8707f0c0abc
Kevin <kevin.pha01@gmail.com>
2024-10-25 18:28:48 -0500

    thumbnails for level selector (menu script half working)

---
+++ Assets/Scripts/ThumbnailScreenshots.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class ThumbnailScreenshots : MonoBehaviour 
+{
+   
+    [MenuItem("DevTools/Take Thumbnail Screenshot for Scene")]
+
+    static void TakeSS()
+    {
+        Camera mainCam = null;
+        Camera thumbnailCam = null;
+        string fileName = SceneManager.GetActiveScene().name.ToString();
+
+        foreach (Camera cam in FindObjectsByType<Camera>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+        {
+            if(cam.name == "Main Camera"){ mainCam = cam; }
+            if(cam.name == "Thumbnail Camera") { thumbnailCam = cam; }
+        }
+        Debug.Assert(mainCam, "No \"Main Camera\" found. Did you do something wrong?");
+        Debug.Assert(thumbnailCam, "No \"Thumbnail Camera\" found. Please create one to capture the thumbnail of the scene");
+        mainCam.gameObject.SetActive(false);
+        thumbnailCam.gameObject.SetActive(true);
+
+        Type gameViewType = Type.GetType("UnityEditor.GameView,UnityEditor");
+        EditorWindow.GetWindow(gameViewType);
+        Directory.CreateDirectory(Application.dataPath + "/Scenes/Thumbnails/");
+        new WaitForSeconds(1);
+        ScreenCapture.CaptureScreenshot(Application.dataPath + "/Scenes/Thumbnails/" + fileName + ".png");
+        Debug.Log("Screenshot saved to " + fileName + ".png");
+        // new WaitForSeconds(1);
+        // mainCam.gameObject.SetActive(true);
+        // thumbnailCam.gameObject.SetActive(false);
+        
+    }
+}
