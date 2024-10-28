30b95ce60665e0fa44a15587e73a7b5c661a9c19
Kevin <kevin.pha01@gmail.com>
2024-10-28 17:39:19 -0500

    redoing scene manager

--- Assets/Scripts/Menus/LevelSelectorMenu.cs
+++ Assets/Scripts/Menus/LevelSelectorMenu.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class LevelSelectorMenu : MonoBehaviour
 {
     public Button playLevelButton;
+    public Button leftNavigateButton;
+    public Button rightNavigateButton;
 
     void Start()
     {
@@ -13,6 +15,6 @@ public class LevelSelectorMenu : MonoBehaviour
     }
 
     public void GoToLevel(){
-        SceneTransitionManager.singleton.GoToSceneAsync(1);
+        SceneTransitionManager.singleton.GoToSceneAsync(1, LoadSceneBy.BuildSettingsOrder);
     }
 }
