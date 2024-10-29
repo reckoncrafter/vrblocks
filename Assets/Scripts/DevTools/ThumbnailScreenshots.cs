5df6491c13631d0ecfe5c5f46f60cba2968b96ee
Kevin <kevin.pha01@gmail.com>
2024-10-28 21:42:41 -0500

    still needs thumbnails

--- Assets/Scripts/DevTools/ThumbnailScreenshots.cs
+++ Assets/Scripts/DevTools/ThumbnailScreenshots.cs
@@ -18,7 +18,7 @@ public class ThumbnailScreenshots : MonoBehaviour
         foreach (Camera cam in FindObjectsByType<Camera>(FindObjectsInactive.Include, FindObjectsSortMode.None))
         {
             if(cam.name == "Main Camera"){ mainCam = cam; }
-            if(cam.name == "Thumbnail Camera") { thumbnailCam = cam; }
+            if(cam.name == "Thumbnail Camera") { thumbnailCam = cam;}
         }
         Debug.Assert(mainCam, "No \"Main Camera\" found. Did you do something wrong?");
         Debug.Assert(thumbnailCam, "No \"Thumbnail Camera\" found. Please create one to capture the thumbnail of the scene");
@@ -30,7 +30,11 @@ public class ThumbnailScreenshots : MonoBehaviour
         Directory.CreateDirectory(Application.dataPath + "/Scenes/Thumbnails/");
         new WaitForSeconds(1);
         ScreenCapture.CaptureScreenshot(Application.dataPath + "/Scenes/Thumbnails/" + fileName + ".png");
+        //TODO: Convert the .png into a Sprite (2D and UI)
+
         Debug.Log("Screenshot saved to " + fileName + ".png");
+        //TODO: properly set active and inactive 
+
         // new WaitForSeconds(1);
         // mainCam.gameObject.SetActive(true);
         // thumbnailCam.gameObject.SetActive(false);
--- Assets/Scripts/Menus/LevelSelectorMenu.cs
+++ Assets/Scripts/Menus/LevelSelectorMenu.cs
@@ -1,5 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,13 +9,54 @@ public class LevelSelectorMenu : MonoBehaviour
     public Button playLevelButton;
     public Button leftNavigateButton;
     public Button rightNavigateButton;
+    public GameObject middleLevelView;
+    public GameObject leftLevelView;
+    public GameObject rightLevelView;
+
+    public int selectedLevelIndex = 1;
+    private Sprite[] levelThumbnails;
 
     void Start()
     {
+        DirectoryInfo thumbnailDir = new DirectoryInfo(Application.dataPath + "/Scenes/Thumbnails");
+        FileInfo[] thumbnailFiles = thumbnailDir.GetFiles("*.png");
+        // levelThumbnails 
+
         playLevelButton.onClick.AddListener(GoToLevel);
+        leftNavigateButton.onClick.AddListener(() => {
+            selectedLevelIndex = Math.Max(0, selectedLevelIndex - 1); 
+            UpdateDisplayView();
+        });
+        rightNavigateButton.onClick.AddListener(() => {
+            selectedLevelIndex = Math.Min(selectedLevelIndex + 1, 5);               //TODO: Change the max value to the # of scenes in the build
+            UpdateDisplayView();
+        });
+        
+        UpdateDisplayView();
     }
 
     public void GoToLevel(){
-        SceneTransitionManager.singleton.GoToSceneAsync(1, LoadSceneBy.BuildSettingsOrder);
+        SceneTransitionManager.singleton.GoToSceneAsync(selectedLevelIndex, LoadSceneBy.BuildSettingsOrder);
+    }
+
+    public void UpdateDisplayView(){
+        // TODO: For error handling, show nothing if there are no levels
+        GameObject middleThumbnail = middleLevelView.transform.Find("LevelThumbnail").GetComponent<GameObject>();
+
+        // Display Left View
+        if(selectedLevelIndex > 0){
+            leftLevelView.SetActive(true);
+        }
+        else{
+            leftLevelView.SetActive(false);
+        }
+        
+        // Display Right View
+        if(selectedLevelIndex < 5){                   //TODO: Change the max value to the # of scenes in the build
+            rightLevelView.SetActive(true);
+        }
+        else{
+            rightLevelView.SetActive(false);
+        }
     }
 }
