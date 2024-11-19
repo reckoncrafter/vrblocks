a8d75179ae93ff72da7a9f78db76d54020fdfbfa
Asa Mimir <dawsfinklea@gmail.com>
2024-11-18 18:45:05 -0600

    Created script to bind block prefabs to turtle movement functions

---
+++ Assets/Map/Turtle/MovementMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TurtleMovement))]
+public class MovementMenu : Editor
+{
+    public override void OnInspectorGUI(){
+        base.OnInspectorGUI();
+        TurtleMovement turtleMovement = (TurtleMovement)target;
+
+        if (GUILayout.Button("Move Forward")){
+            turtleMovement.WalkForward();
+        }
+        if (GUILayout.Button("Turn Left")){
+            turtleMovement.RotateLeft();
+        }
+        if (GUILayout.Button("Turn Right")){
+            turtleMovement.RotateRight();
+        }
+    }
+}
