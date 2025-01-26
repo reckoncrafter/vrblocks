bf3422a40683c61ee43974466b7028c032673a1c
Kevin <kevin.pha01@gmail.com>
2025-01-26 01:21:44 -0600

    note

--- Assets/Scripts/DevTools/ToggleXRDeviceManager.cs
+++ Assets/Scripts/DevTools/ToggleXRDeviceManager.cs
@@ -5,6 +5,7 @@ public class ToggleXRDeviceManager: MonoBehaviour
 {
     public void Awake()
     {
+      //TODO: If production build, must default to false
       if(ToggleXRDeviceEmulator.useXRDeviceEmulator){
         this.GameObject().SetActive(true);
       }
