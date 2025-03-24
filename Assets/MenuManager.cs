1b3be5c9e8bf2aafe6091994897603218b139d2c
JLaw <johnathonlawton15@gmail.com>
2025-03-24 00:17:09 -0500

    Creation
    I'm going to lock each tab in the block menu based on level completion using some existing functions created by Kevin. Currently this just works as Movement being unlocked by default, Control being unlocked with level 2, and Function being unlocked with level 3. It's really easy to change this if needed.

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public Button movementTab;
+    public Button controlTab;
+    public Button functionTab;
+
+    void Start()
+    {
+        int levelProgress = SceneTransitionStates.GetSelectedLevel(); // Get current level
+        UpdateTabLocks(levelProgress);
+    }
+
+    public void UpdateTabLocks(int level)
+    {
+        movementTab.interactable = true;
+        controlTab.interactable = level >= 2;
+        functionTab.interactable = level >= 3;
+    }
+}
