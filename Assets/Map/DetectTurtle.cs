0f10a689c5e26b5802bbfc46225bc6762988ecd4
Asa Mimir <dawsfinklea@gmail.com>
2025-03-25 16:48:13 -0500

    removed some unnecessary directives

--- Assets/Map/DetectTurtle.cs
+++ Assets/Map/DetectTurtle.cs
@@ -1,6 +1,4 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class DetectTurtle
 {
--- Assets/Map/Turtle/TurtleMovement.cs
+++ Assets/Map/Turtle/TurtleMovement.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using TMPro;
 
 public class TurtleMovement : MonoBehaviour
 {
