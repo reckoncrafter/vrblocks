4a03760fc7b4dc2cd3fb302a398bb7e81226555f
Asa Mimir <dawsfinklea@gmail.com>
2025-04-08 00:28:33 -0500

    added checks to prevent the Start Queue block and MiniMap from being deleted. To this end, removed colliders on child objects of MiniMapSpawner. Changed TriggerAudio to listen to an event from DeletionBoundary.

--- Assets/Sounds/TriggerAudio.cs
+++ Assets/Sounds/TriggerAudio.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class TriggerAudio : MonoBehaviour
 {
     private AudioSource audio;
+    private DeletionBoundary deletionBoundary;
     void Start()
     {
         audio = GetComponent<AudioSource>();
+        if(TryGetComponent(out deletionBoundary))
+        {
+            deletionBoundary.objectDestroyedEvent.AddListener(OnObjectDestroyed);
+        }
     }
 
-    void OnTriggerEnter()
+    void OnObjectDestroyed()
     {
         audio.Play();
     }
